509432e46abcb9db1960e2db8986c6bdac0cde1d
Ben Rush <orionrush@gmail.com>
2022-10-07 00:24:33 +0100

    much changes

--- dist/Satchel.js
+++ dist/Satchel.js
@@ -4,38 +4,182 @@
  *
  */
 class Satchel {
-  #stcl = 'stcl'
+  #cargo
   #pocketKey
-  #storage
+  #store
+  #settings
 
   /**
    * @class Satchel
    * @classdec Create new Satchel instance
    * @param {string} key Storage key
-   * @param {object} cargo The data to be saved to Storage
-   * @param {boolean} local Specify sessionStorage (default) or localStorage
-   * @param {string} pocket Namespace for Storage keys
+   * @param {object} cargo The cargo payload to be saved to Storage
+   * @property {object|string} cargo.data The data to be saved, may be string or object
+   * @property {number|null} cargo.expiry The expiry time of the cargo ,may be a number or null
+   * @param {boolean} [local=false] Specify sessionStorage (default) or localStorage
+   * @param {string} [pocket='pocket'] Namespace for Storage keys, default is 'pocket'
    */
-  constructor(key = null, cargo = {}, local = false, pocket = 'satchel') {
-    this.#storage = local ? localStorage : sessionStorage;
+  constructor(key = null, cargo = {}, local = false, pocket = 'pocket') {
+    this.#store = local ? window.localStorage : window.sessionStorage;
+    if (!key) throw new Error('Satchel: a key is required.')
+    this.stcl = Satchel.stcl;
+    this.#pocketKey = `${this.stcl}.${pocket}.${key}`;
+    this.#settings = { data: undefined, expiry: null };
+    this.#cargo = { ...this.#settings, ...cargo };
 
-    if (!this.#storageAvailable(this.#storage)) return false
-    if (!key) throw new Error('Satchel: a Key is required.')
-
-    this.#pocketKey = `${this.#stcl}.${this.pocket}.${this.key}`;
-
-    this.settings = { data: undefined, expiry: null };
-    this.cargo = { ...this.settings, ...cargo };
-
+    if (typeof this.#cargo.expiry !== 'number' && this.#cargo.expiry !== null) {
+      throw new Error('Satchel: Expiry must be null or a number.')
+    }
     // Keep creation time from being edited
-    Object.defineProperty(this.cargo, 'creation', {
+    Object.defineProperty(this.#cargo, 'creation', {
       enumerable: true,
       writable: false,
       value: Date.now()
     });
 
-    // Send the cargo to Storage
-    this.set(this.cargo);
+    // set the cargo in Storage
+    this.set(this.#cargo);
+  }
+
+  /**
+   * Returns an object of age and freshness related data
+   *
+   * @typedef {Object} Age object
+   * @property {number} age in milliseconds
+   * @property {number} creation Date.now() (ms)src/Satchel.js
+   * @property {boolean} fresh if Store key is fresh
+   * @return {Age} An age object representing the age of the current Store key.
+   */
+  age() {
+    const store = JSON.parse(this.#store.getItem(this.#pocketKey));
+    return {
+      age: Date.now() - store.creation,
+      creation: store.creation,
+      expiry: store.expiry,
+      fresh: this.isFresh()
+    }
+  }
+
+  /**
+   *
+   * @returns {null | boolian:false } nullif su
+   */
+  bin() {
+    this.#store.removeItem(this.#pocketKey);
+    if (!this.#store.getItem(this.#pocketKey)) {
+      Satchel.#emit({
+        type: this.#store,
+        key: this.#pocketKey,
+        action: 'bin'
+      });
+      return null
+    } else {
+      throw new Error('Satchel: Failure to bin key: ' + this.#pocketKey)
+    }
+  }
+
+  get(ignore = false) {
+    const item = this.#store.getItem(this.#pocketKey);
+    if (!item) return false
+    if ((this.isFresh() && !ignore) || ignore) {
+      return JSON.parse(item)
+    }
+    return false
+  }
+
+  /**
+   * Sets the data, for a given key and namespace
+   *
+   * @param {object} settings object
+   * @property {object|string} data the Storage object to set
+   * @property {number|null} expiry the expiery date in (ms)
+   * @property {number} creation the creation date in (ms) Date.now()
+   *
+   * @return {Object} Satchel
+   */
+  set({ data, expiry, creation }) {
+    if (typeof this.#cargo.expiry !== 'number' && this.#cargo.expiry !== null) {
+      throw new Error('Satchel: Expiry must be null or a number.')
+    }
+    if (typeof this.#cargo.creation !== 'number') {
+      throw new Error(
+        'Satchel: Creation must be null or a number, it should not be set directly.'
+      )
+    }
+    const storedEntry = this.get(true);
+    const temp = {};
+    temp.data = data || null; // Key exsists but we are not setting a data attribute
+    temp.expiry = expiry || this.#cargo?.expiry || null;
+
+    if (!data && !expiry && storedEntry)
+      throw new Error(
+        `Satchel: The key ("${this.#pocketKey}") already exists in ${
+          this.#store === window.localStorage
+            ? 'LocalStorage'
+            : 'SessionStorage'
+        }, and "data" and "expiry" atributes have not been set, set these or create a new unique key.`
+      )
+    // Key exsists and the creation property is being changed
+    if (!data && storedEntry && creation) {
+      throw new Error(
+        'Satchel: Overwriting a key\'s "creation" property is not allowed.'
+      )
+    } else {
+      // dont overwrite existing creation time
+      temp.creation = storedEntry?.creation || this.#cargo.creation;
+    }
+    // Set storage values
+    this.#store.setItem(this.#pocketKey, JSON.stringify(temp));
+
+    // Update internal reference object
+    this.#cargo = { ...this.#cargo, ...temp };
+
+    Satchel.#emit({
+      type: this.#store,
+      key: this.#pocketKey,
+      action: 'set',
+      data: temp,
+      oldData: storedEntry
+    });
+    return this
+  }
+
+  /**
+   * Check if current item is fresh.
+   *
+   * @returns {boolean} true if the item has not expired
+   */
+  isFresh() {
+    const store = JSON.parse(this.#store.getItem(this.#pocketKey));
+    return !store?.expiry ? true : store.expiry - Date.now() > 0
+  }
+
+  /**
+   * Returns the current namespaced Store key (pocket.key), the key is prefixed with 'stcl'
+   *
+   * @returns string
+   */
+  getKey() {
+    return this.#pocketKey
+  }
+
+  /**
+   * Returns array of keys for the a 'Pocket' namespace.
+   *
+   * @param {string} pocket namespace prefix, default 'pocket'
+   * @param {boolean} local specify sessionStorage (default) or localStorage
+   * @returns {array} array of Storage keys for the current pocket.
+   */
+  static getAllPocketKeys(pocket = 'pocket', local = false) {
+    const store = local ? window.localStorage : window.sessionStorage;
+
+    return Object.keys(store)
+      .map((key) => {
+        return key.startsWith(this.stcl + '.' + pocket) ? key : ''
+      })
+      .filter(function (e) {
+        return e
+      })
   }
 
   /**
@@ -51,7 +195,7 @@ class Satchel {
    * @property {string | null} detail.data the stringified data for the given key
    * @returns {CustomEvent} CustomEvent
    */
-  #emit(detail) {
+  static #emit(detail) {
     const required = {
       type: null,
       key: null,
@@ -68,188 +212,24 @@ class Satchel {
   }
 
   /**
-   * Todo: replace with https://www.npmjs.com/package/storage-available
+   * Clears all items regardless of freshness from a given 'pocket' namespace
    *
-   * Borrowed humbly from https://www.npmjs.com/package/storage-available
-   * @returns {boolean} true if the environment has localStorage
-   */
-  #storageAvailable(type) {
-    // let SatchelStorgeTest
-
-    // const key = '__testSatchelEnv__'
-    // if (SatchelStorgeTest !== undefined) return false
-    // try {
-    //   if (!localStorage) return false
-    // } catch (e) {
-    //   console.warn(e)
-    //   return false
-    // }
-    // try {
-    //   localStorage.setItem(key, key)
-    //   localStorage.removeItem(key, key)
-    //   SatchelStorgeTest = true
-    // } catch (e) {
-    //   SatchelStorgeTest = !!(this.#isOutOfSpace(e) && localStorage.length)
-    // }
-    // return SatchelStorgeTest
-
-    try {
-      storageAvailable(type);
-    } catch (e) {
-      if (!this.#storageFull(type)) return false
-    }
-  }
-
-  /**
-   * Borrowed humbly from https://github.com/pamelafox/lscache/blob/master/lscache.js
-   * @returns {boolean} true if the environment has localStorage
-   */
-  #storageFull(e) {
-    return (
-      e &&
-      (e.name === 'QUOTA_EXCEEDED_ERR' ||
-        e.name === 'NS_ERROR_DOM_QUOTA_REACHED' ||
-        e.name === 'QuotaExceededError')
-    )
-  }
-
-  /**
-   * Returns an object of age and freshness related data
-   *
-   * @typedef {Object} Age object
-   * @property {number} age in milliseconds
-   * @property {number} creation Date.now() (ms)
-   * @property {boolean} fresh if Store key is fresh
-   * @return {Age} An age object representing the age of the current Store key.
-   */
-  age() {
-    const store = JSON.parse(this.#storage.getItem(this.#pocketKey));
-    return {
-      age: Date.now() - store.creation,
-      creation: store.creation,
-      expiry: store.expiry,
-      fresh: this.isFresh()
-    }
-  }
-
-  /**
-   *
-   * @returns {null | boolian:false } nullif su
-   */
-  bin() {
-    this.#storage.removeItem(this.#pocketKey);
-
-    if (!this.#storage.getItem(this.#pocketKey)) {
-      this.#emit({
-        type: this.#storage,
-        key: this.#pocketKey,
-        action: 'bin'
-      });
-      return null
-    }
-  }
-
-  get(ignore = false) {
-    const item = this.#storage.getItem(this.#pocketKey);
-    if (this.isFresh() && !ignore) {
-      return item
-    } else if (ignore) {
-      return JSON.parse(this.#storage.getItem(this.#pocketKey))
-    }
-
-    this.#emit({
-      type: this.#storage,
-      key: this.#pocketKey,
-      action: 'get',
-      data: item
-    });
-
-    return this
-  }
-
-  /**
-   * Sets the data, for a given key and namespace
-   *
-   * @param {Object} settings object
-   * @property {Object} data the Storage object to set
-   * @property {number} expiry the expiery date in (ms)
-   * @property {string} creation the creation date in (ms) Date.now()
-   *
-   * @return {Object} Satchel
-   */
-  set({ data, expiry, creation }) {
-    const current = this.get(true);
-    const temp = {};
-    temp.data = data || null;
-    temp.expiry = expiry || this.cargo?.expiry || null;
-    temp.creation = current?.creation || this.cargo.creation; // dont overwrite existing creation time
-
-    this.#storage.setItem(this.#pocketKey, JSON.stringify(temp));
-
-    this.#emit({
-      type: this.#storage,
-      key: this.#pocketKey,
-      action: 'set',
-      data: temp
-    });
-    return this
-  }
-
-  /**
-   * Check if current item is fresh.
-   *
-   * @returns {boolean} true if the item has not expired
-   */
-  isFresh() {
-    const store = JSON.parse(this.#storage.getItem(this.#pocketKey));
-    return !store?.expiry ? true : store.expiry - Date.now() > 0
-  }
-
-  /**
-   * Returns the current namespaced Store key (pocket.key)
-   *
-   * @returns string
-   */
-  getID() {
-    return this.#pocketKey
-  }
-
-  /**
-   * TODO: remove .includes for a more robust check
-   *
-   * Returns array of keys for the a 'pocket' namespace.
-   *
-   * @param {string} pocket namespace prefix, default 'satchel'
+   * @param {string} pocket namespace prefix, default 'pocket'
    * @param {boolean} local specify sessionStorage (default) or localStorage
-   * @returns {array} array of Storage keys for the current pocket.
+   * @returns {array} The number of items remaing in store, which if sucessful should be 0.
    */
-  static getAllPocketKeys(pocket = 'satchel', local = false) {
-    return Object.keys(localStorage)
-      .map((key) => {
-        return key.startsWith(this.#stcl + '.' + pocket) ? key : ''
-      })
-      .filter(function (e) {
-        return e
-      })
-  }
+  static emptyPocket(pocket = 'pocket', local = false) {
+    const store = local ? window.localStorage : window.sessionStorage;
+    const keysBefore = Satchel.getAllPocketKeys(pocket, local);
+    if (!keysBefore.length) {
+      console.warn('No pocket keys found for ' + pocket + ' in ' + store);
+      return keysBefore
+    }
 
-  /**
-   * Clears all items in a specified 'pocket' namespace, default 'satchel'
-   *
-   * @param {string} pocket namespace prefix, default 'satchel'
-   * @param {boolean} local specify sessionStorage (default) or localStorage
-   * @returns {numbererger} The number of items remaing in store, which if sucessful should be 0.
-   */
-  static emptyPocket(pocket = 'satchel', local = false) {
-    const store = local ? localStorage : sessionStorage;
-    const items = Satchel.getAllPocketKeys(this.#stcl + '.' + pocket, store);
-    if (!items.length) return null
-
-    Object.keys(items).forEach((item) => {
-      store.removeItem(items[item]);
+    Object.keys(keysBefore).forEach((key) => {
+      store.removeItem(keysBefore[key]);
     });
-
-    const remaing = Satchel.getAllPocketKeys(pocket, store).length;
+    const keysRemaining = Satchel.getAllPocketKeys(pocket, local);
 
     document.dispatchEvent(
       new CustomEvent('Satchel', {
@@ -257,67 +237,66 @@ class Satchel {
         cancelable: true,
         detail: {
           action: 'emptyPocket',
-          remaing
+          keysBefore,
+          keysRemaining
         }
       })
     );
-
-    return remaing
+    return keysRemaining
   }
 
   /**
-   *  Destroys all expired items from a given 'pocket' namespace
+   *  Removes all expired items from a given 'pocket' namespace
    *
-   * @param {string} pocket namespace prefix, default 'satchel'
+   * @param {string} pocket namespace prefix, default 'pocket'
    * @param {boolean} local specify sessionStorage (default) or localStorage
    * @returns The number of items remaing in store that have yet to expire.
    */
-  static cleanPocket(pocket = 'satchel', local = false) {
+  static tidyPocket(pocket = 'pocket', local = false) {
     const store = local ? localStorage : sessionStorage;
-    const items = Satchel.getAllPocketKeys(pocket, store);
-    if (!items.length) return null
+    const keysBefore = Satchel.getAllPocketKeys(pocket, local);
+    if (!keysBefore.length) return null
 
-    Object.keys(items).forEach((item) => {
-      const expiry = JSON.parse(store.getItem(items[item])).expiry;
+    Object.keys(keysBefore).forEach((key) => {
+      const expiry = JSON.parse(store.getItem(keysBefore[key])).expiry;
       if (!expiry || expiry - Date.now() > 0) return null
-      store.removeItem(items[item]);
+      store.removeItem(keysBefore[key]);
     });
-    const remaing = Satchel.getAllPocketKeys(
-      this.#stcl + '.' + pocket,
-      store
-    ).length;
-
+    const keysRemaining = Satchel.getAllPocketKeys(pocket, local).length;
     document.dispatchEvent(
-      new CustomEvent('SatchelStatic', {
+      new CustomEvent('Satchel', {
         bubbles: true,
         cancelable: true,
         detail: {
-          action: 'cleanPocket',
-          remaing
+          action: 'tidyPocket',
+          keysBefore: keysBefore.length,
+          keysRemaining
         }
       })
     );
 
-    return remaing
+    return keysRemaining
   }
 
   /**
-   * Returns an instance of Satchel associated with the provided key, or false.
+   * Returns an instance of Satchel associated with the given key, pocket,store, or false if none is found.
    *
-   * @param {string} key  key for target item
-   * @param {string} pocket namespace prefix, default 'satchel'
-   * @param {string} local specify sessionStorage (default) or localStorage
-   * @returns {Satchel} new Satchel instance | false
+   * @param {string} key key for the stored item
+   * @param {string} [pocket='pocket'] namespace prefix, default 'pocket'
+   * @param {string} [local='false'] specify sessionStorage (default) or localStorage
+   * @returns {Satchel|false} new Satchel instance | false
    */
-  static getKey(key, pocket = 'satchel', local = false) {
-    const pocketKey = `${this.#stcl}.${this.pocket}.${this.key}`;
+  static getSatchel(key, pocket = 'pocket', local = false) {
+    const pocketKey = `${Satchel.stcl}.${pocket}.${key}`;
     const store = local ? localStorage : sessionStorage;
-    const item = store.getItem(pocketKey);
+    const item = JSON.parse(store.getItem(pocketKey));
     if (!item) return false
 
-    return new Satchel(key, null, local, pocket)
+    return new Satchel(key, item, local, pocket)
   }
 }
+
+Satchel.stcl = 'stcl';
 
 export { Satchel };
 //# sourceMappingURL=Satchel.js.map
