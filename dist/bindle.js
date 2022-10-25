40868589c0a16419c0e6e4d469f8b1caf2da422a
Ben Rush <orionrush@gmail.com>
2022-10-25 13:10:52 +0100

    bump

--- dist/bindle.js
+++ dist/bindle.js
@@ -1,5 +1,5 @@
 
-/* ! @preserve @bnjmnrsh/satchel v0.2.0 | (c) 2022 bnjmnrsh | ISC | https://github.com/bnjmnrsh/satchel */
+/* ! @preserve @bnjmnrsh/satchel v0.2.1 | (c) 2022 bnjmnrsh | ISC | https://github.com/bnjmnrsh/satchel */
 /**
  * Tools for managing Satchel pockets.
  */
